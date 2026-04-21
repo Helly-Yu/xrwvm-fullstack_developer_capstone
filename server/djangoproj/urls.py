d01ed9e0606e0fd2372efc1f3067b09b225988ea
Helly-Yu <wyuuuu0509@gmail.com>
2026-04-21 08:53:34 -0400

    Create a Dealer Details or Reviews Page

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('dealer/<int:dealer_id>/',TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>/',TemplateView.as_view(template_name="index.html")),
     path('manifest.json', TemplateView.as_view(template_name="manifest.json", content_type='application/json')),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
